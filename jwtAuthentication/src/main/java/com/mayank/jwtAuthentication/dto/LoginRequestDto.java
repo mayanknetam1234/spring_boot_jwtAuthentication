325229b37820868fd8b0cbaadd442fa51ad53117
mayanknetam1234 <netammayank547@gmail.com>
2025-07-29 19:20:34 +0530

    changes application properties

--- jwtAuthentication/src/main/java/com/mayank/jwtAuthentication/dto/LoginRequestDto.java
+++ jwtAuthentication/src/main/java/com/mayank/jwtAuthentication/dto/LoginRequestDto.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class LoginRequestDto {
-    private String email;
+    private String username;
     private String password;
 }
